bc672373ad6e188e9179ed14b0f6fe619d4c28cb
Bartosz Kogut <dervishbk@gmail.com>
2021-01-03 15:11:47 +0100

    helloworld

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Bartosz";
+        int myAge = 28;
+        double height = 1.81;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+    }
+}
