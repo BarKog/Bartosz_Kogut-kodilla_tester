d3633da1a5c774b4b8d8e0772739ba371e4c0cb2
Bartosz Kogut <dervishbk@gmail.com>
2021-01-04 15:03:00 +0100

    SimpleArray

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,16 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animals = new String[5];
+        animals[0] = "Dog";
+        animals[1] = "Cat";
+        animals[2] = "Tiger";
+        animals[3] = "Horse";
+        animals[4] = "Bear";
+
+
+        String arrayc3 = animals[3];
+
+        System.out.println(arrayc3);
+
+        System.out.println(animals[0]);
+    }}
